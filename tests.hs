a510559380c82dc309e595368725a9706a0c9a30
Anderson Barrera <andersonbar10@gmail.com>
2025-02-27 00:25:24 -0500

    [UPD] Test Cases

--- tests.hs
+++ tests.hs
@@ -17,12 +17,12 @@ main = do
         (Query ["nombre", "edad"] "usuarios" Nothing)
     
     -- Prueba 2: Consulta con WHERE
-    runTest "Prueba 2: Consulta con WHERE" "SELECT nombre FROM usuarios WHERE edad > 30;;"
+    runTest "Prueba 2: Consulta con WHERE" "SELECT nombre FROM usuarios WHERE edad > 30;"
         (Query ["nombre"] "usuarios" (Just (Cond (Condition "edad" Gt "30"))))
     
-    -- Prueba 3: Consulta con AND y OR
-    runTest "Prueba 3: WHERE con AND y OR" "SELECT nombre FROM usuarios WHERE edad > 30 AND ciudad = Madrid;"
-        (Query ["nombre"] "usuarios" (Just (And (Cond (Condition "edad" Gt "30")) (Cond (Condition "ciudad" Eq "Madrid")))))
+    -- Prueba 3: WHERE con AND y OR
+    runTest "Prueba 3: WHERE con AND y OR" "SELECT nombre FROM usuarios WHERE edad > 30 AND ciudad = Madrid OR salario >= 5000;"
+        (Query ["nombre"] "usuarios" (Just (Or (And (Cond (Condition "edad" Gt "30")) (Cond (Condition "ciudad" Eq "Madrid"))) (Cond (Condition "salario" Ge "5000")))))
     
     -- Prueba 4: Comparadores (=, !=, >=, <=, >, <)
     runTest "Prueba 4.1: WHERE con operador =" "SELECT nombre FROM usuarios WHERE edad = 30;"
@@ -35,5 +35,9 @@ main = do
         (Query ["nombre"] "usuarios" (Just (Cond (Condition "edad" Ne "30"))))
     runTest "Prueba 4.5: WHERE con operador <" "SELECT nombre FROM usuarios WHERE edad < 30;"
         (Query ["nombre"] "usuarios" (Just (Cond (Condition "edad" Lt "30"))))
-    runTest "Prueba 4.6: WHERE con operador >" "SELECT nombre FROM usuarios WHERE edad > 30"
+    runTest "Prueba 4.6: WHERE con operador >" "SELECT nombre FROM usuarios WHERE edad > 30;"
         (Query ["nombre"] "usuarios" (Just (Cond (Condition "edad" Gt "30"))))
+    
+    -- Prueba 5: WHERE con múltiples AND y OR
+    runTest "Prueba 6: WHERE con combinación de AND y OR" "SELECT nombre FROM empleados WHERE edad > 25 AND departamento = Ventas OR salario > 5000 AND experiencia >= 5;"
+        (Query ["nombre"] "empleados" (Just (Or (And (Cond (Condition "edad" Gt "25")) (Cond (Condition "departamento" Eq "Ventas"))) (And (Cond (Condition "salario" Gt "5000")) (Cond (Condition "experiencia" Ge "5"))))))
